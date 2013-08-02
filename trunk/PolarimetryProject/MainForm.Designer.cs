8b054c7be324d4fb20cbfebaa903953bf95b03bf
andriy.krupych@gmail.com <andriy.krupych@gmail.com@ffd2edeb-dafa-466c-0fa5-29f81c8a97c8>
2013-08-02 11:40:05 +0000

    Added some menu controls

---
+++ trunk/PolarimetryProject/MainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace PolarimetryProject
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.menuButtonOpen = new System.Windows.Forms.ToolStripButton();
+            this.menuLabelPath = new System.Windows.Forms.ToolStripLabel();
+            this.menuLabelPosition = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuButtonOpen,
+            this.menuLabelPath,
+            this.menuLabelPosition,
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(284, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // menuButtonOpen
+            // 
+            this.menuButtonOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.menuButtonOpen.Image = ((System.Drawing.Image)(resources.GetObject("menuButtonOpen.Image")));
+            this.menuButtonOpen.ImageTransparentColor = System.Drawing.Color.White;
+            this.menuButtonOpen.Name = "menuButtonOpen";
+            this.menuButtonOpen.Size = new System.Drawing.Size(23, 22);
+            this.menuButtonOpen.Text = "toolStripButton1";
+            // 
+            // menuLabelPath
+            // 
+            this.menuLabelPath.Name = "menuLabelPath";
+            this.menuLabelPath.Size = new System.Drawing.Size(0, 22);
+            // 
+            // menuLabelPosition
+            // 
+            this.menuLabelPosition.Name = "menuLabelPosition";
+            this.menuLabelPosition.Size = new System.Drawing.Size(0, 22);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Enabled = false;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.White;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Enabled = false;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.White;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "MainForm";
+            this.Text = "Polarimetry";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton menuButtonOpen;
+        private System.Windows.Forms.ToolStripLabel menuLabelPath;
+        private System.Windows.Forms.ToolStripLabel menuLabelPosition;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+
+    }
+}
+
--- trunk/PolarimetryProject/Program.cs
+++ trunk/PolarimetryProject/Program.cs
@@ -15,7 +15,7 @@ namespace PolarimetryProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
